65c14d21286d4c98983f50494f00a0fa777c0ddd
Zulu <juanpablozuluaga.0311@gmail.com>
2025-04-11 22:50:20 -0500

    decripcion en las card

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -5,6 +5,7 @@ interface ProductCardProps {
     id: number;
     nombre: string;
     precio: number;
+    descripcion: string;
     imagen_url: string;
   };
 }
@@ -18,6 +19,7 @@ function ProductCard({ product }: ProductCardProps) {
         className="w-full h-48 object-cover rounded"
       />
       <h2 className="text-lg font-semibold mt-2">{product.nombre}</h2>
+      <p className="text-gray-600 mb-2">{product.descripcion}</p>
       <p className="text-gray-600 mb-2">${product.precio}</p>
       <Link href={`/compra/${product.id}`}>
         <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
--- src/app/components/ProductPagar.tsx
+++ src/app/components/ProductPagar.tsx
@@ -18,6 +18,8 @@ function ProductPagar({ product }: ProductCardProps) {
         className="w-full h-48 object-cover rounded"
       />
       <h2 className="text-lg font-semibold mt-2">{product.nombre}</h2>
+      <h2 className="text-lg font-semibold mt-2"></h2>
+
       <p className="text-gray-600 mb-2">${product.precio}</p>
       <Link href={`/pasarelaPayU/${product.id}`}>
         <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 m">
